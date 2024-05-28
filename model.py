78c8fb5e1c555a0978d57d218d902231affc082c
Arturo Suárez <manuel.suarez@cimat.mx>
2024-05-27 20:43:42 -0600

    Fixes

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms.functional as try:
-    
+import torchvision.transforms.functional as TF
+
 
 class UNET(nn.Module):
     def __init__(self, in_channels=3, classes=1):
@@ -9,15 +9,25 @@ class UNET(nn.Module):
         self.layers = [in_channels, 64, 128, 256, 512, 1024]
 
         self.double_conv_downs = nn.ModuleList(
-            [self.__double_conv(layer, layer_n) for layer, layer_n in zip(self.layers[:-1], self.layers[1:])])
+            [
+                self.__double_conv(layer, layer_n)
+                for layer, layer_n in zip(self.layers[:-1], self.layers[1:])
+            ]
+        )
 
         self.up_trans = nn.ModuleList(
-            [nn.ConvTranspose2d(layer, layer_n, kernel_size=2, stride=2)
-                for layer, layer_n in zip(self.layers[::-1][:-2], self.layers[::-1][1:-1])])
+            [
+                nn.ConvTranspose2d(layer, layer_n, kernel_size=2, stride=2)
+                for layer, layer_n in zip(
+                    self.layers[::-1][:-2], self.layers[::-1][1:-1]
+                )
+            ]
+        )
 
         self.double_conv_ups = nn.ModuleList(
-            [self.__double_conv(layer, layer//2) for layer in self.layers[::-1][:-2]])
-        
+            [self.__double_conv(layer, layer // 2) for layer in self.layers[::-1][:-2]]
+        )
+
         self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.final_conv = nn.Conv2d(64, classes, kernel_size=1)
@@ -28,7 +38,7 @@ class UNET(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=2, padding=1),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
         return conv
 
@@ -45,7 +55,9 @@ class UNET(nn.Module):
         concat_layers = concat_layers[::-1]
 
         # up layers
-        for up_trans, double_conv_up, concat_layers in zip(self.up_trans, self.double_conv_ups, concat_layers):
+        for up_trans, double_conv_up, concat_layer in zip(
+            self.up_trans, self.double_conv_ups, concat_layers
+        ):
             x = up_trans(x)
             if x.shape != concat_layer.shape:
                 x = TF.resize(x, concat_layer.shape[2:])
